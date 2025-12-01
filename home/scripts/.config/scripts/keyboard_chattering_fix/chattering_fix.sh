ae58ad254fc2be23a4c8e5c282e1edd0b85f7a94
rlegacy <rdl0011@gmail.com>
2025-12-01 10:17:34 -0500

    keyboard chattering fix

---
+++ home/scripts/.config/scripts/keyboard_chattering_fix/chattering_fix.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# Change the line below to the absolute path of the folder
+cd /home/rlegacy/.config/scripts/keyboard_chattering_fix/ && sudo uv run python3 -m src -k usb-Corsair_CORSAIR_SLIPSTREAM_WIRELESS_USB_Receiver_B1EF71C3D905BDAC-if03-event-kbd -t 50
